f94221e38d5e27e9872104aba375b5acf3256d3b
Rishabh <rishabhbhatt956@gmail.com>
2024-12-17 12:08:10 +0530

    Home screen meditations list

--- src/components/MeditaionListItem.tsx
+++ src/components/MeditaionListItem.tsx
@@ -1,10 +1,24 @@
 import {View, Text} from 'react-native'
 import { Meditation } from "@/types";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export function MeditationListItem({ meditation }: {meditation: Meditation}){
     return(
-        <View className="p-5 border-2 border-gray-300 rounded-3xl">
-            <Text className="font-semibold text-2xl">{meditation.title}</Text>
+        <View className='flex-row pr-5 items-center gap-5'>
+            <View className='bg-green-700 p-2 rounded-full'>
+                <FontAwesome name="check" size={18} color="white" />    
+            </View>
+
+            <View className="flex-1 p-5 border-2 border-gray-300 rounded-3xl">
+                <Text className="font-semibold text-2xl mb-2">{meditation.title}</Text>
+                
+                <View className='flex-row pr-5 items-center gap-2'>
+                    <FontAwesome6 name="clock" size={16} color="#6B7280" />
+                    <Text className='text-gray-600 '>{meditation.duration} min</Text>
+                </View>
+            </View>
         </View>
+        
     )
 }
